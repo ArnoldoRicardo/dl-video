ce8d029d6759c338218cb086f56528986ff1afd9
ArnoldoRicardo <1000317+ArnoldoRicardo@users.noreply.github.com>
2023-10-07 13:31:17 -0600

    feat: Add MongoDB connection and configuration settings

--- src/config.py
+++ src/config.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     TOKEN: str = 'DUMY_TOKEN'
     MONGO_INITDB_ROOT_USERNAME: str = 'root'
     MONGO_INITDB_ROOT_PASSWORD: str = 'example'
+    MONGO_HOST: str = 'localhost'
+    MONGO_PORT: str = '27017'
 
     class Config:
         env_file = '.env'
