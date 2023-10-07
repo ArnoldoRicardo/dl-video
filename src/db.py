45ad8d9fc7d4031a0bf98913dc4790b721edc189
ArnoldoRicardo <1000317+ArnoldoRicardo@users.noreply.github.com>
2023-10-07 14:07:05 -0600

    feat(db): Add functions to create/update/delete/find users and chats

--- src/db.py
+++ src/db.py
@@ -1,9 +1,11 @@
-from pymongo import MongoClient
-from src.config import settings
-import os
 import logging
 
-connection_string = f"mongodb://{settings.MONGO_INITDB_ROOT_USERNAME}:{settings.MONGO_INITDB_ROOT_PASSWORD}@{settings.MONGO_HOST}:{settings.MONGO_PORT}/"
+from pymongo import MongoClient
+
+from src.config import settings
+from src.schemas import ChatModel, UserModel
+
+connection_string = f"mongodb://{settings.MONGO_INITDB_ROOT_USERNAME}:{settings.MONGO_INITDB_ROOT_PASSWORD}@{settings.MONGO_HOST}:{settings.MONGO_PORT}/"  # noqa
 
 
 logging.basicConfig(
@@ -20,5 +22,30 @@ except Exception as e:
     logger.error(e)
     raise e
 
-def get_database(name: str) -> MongoClient:
-    return client[name]
+db = client['dl-video-bot']
+users_collection = db['users']
+chats_collection = db['chats']
+
+
+def create_or_update_user(user: UserModel):
+    users_collection.update_one({"id": user.id}, {"$set": user.dict()}, upsert=True)
+
+
+def delete_user_by_id(user_id: int):
+    users_collection.delete_one({"id": user_id})
+
+
+def find_user_by_id(user_id: int):
+    return users_collection.find_one({"id": user_id})
+
+
+def create_or_update_chat(chat: ChatModel):
+    chats_collection.update_one({"id": chat.id}, {"$set": chat.dict()}, upsert=True)
+
+
+def find_chat_by_id(chat_id: int):
+    return chats_collection.find_one({"id": chat_id})
+
+
+def delete_chat_by_id(chat_id: int):
+    chats_collection.delete_one({"id": chat_id})
